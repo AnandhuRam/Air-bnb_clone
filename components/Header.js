30b4bc9b7798bfbb2d4f3eb2e69321be2a074751
= <anandhuram2021@gmail.com>
2021-09-24 17:42:13 +0530

    image config done

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <Image
+                    src = " https://links.papareact.com/qd3"
+                    layout='fill'
+                />
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
