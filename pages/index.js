dbc5cfc775e1415192b1b91832c71ca69c036aa9
= <anandhuram2021@gmail.com>
2021-10-19 12:12:27 +0530

    frontend day1 cpmlpted

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Largecard from '../components/Largecard'
 import Mediumcard from '../components/Mediumcard'
@@ -47,6 +48,7 @@ export default function Home({exploreData,cardData}) {
               buttontext="Get Inspired"
              />
         </menu>
+        <Footer/>
 
       
     </div>
